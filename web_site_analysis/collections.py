9cdc854c68d86082502e944570b8005f84f11792
serhii <ockar2018@gmail.com>
2024-11-15 12:30:22 +0200

    create collections.py file

---
+++ web_site_analysis/collections.py
@@ -0,0 +1,21 @@
+MONTHS_UA = {
+    "січня": 1,
+    "лютого": 2,
+    "березня": 3,
+    "квітня": 4,
+    "травня": 5,
+    "червня": 6,
+    "липня": 7,
+    "серпня": 8,
+    "вересня": 9,
+    "жовтня": 10,
+    "листопада": 11,
+    "грудня": 12
+}
+
+TECHNOLOGIES = {
+    "Python", "Git", "SQL", "REST API", "Docker", "AWS", "Linux", "Django",
+    "PostgreSQL", "Artificial Intelligence", "JS", "Machine Learning", "React",
+    "OOP", "Flask", "NoSQL", "Networking", "Fullstack", "Microservice", "MongoDB",
+    "HTML", "CSS", "Algorithms", "DRF", "FastAPI", "Asyncio", "GraphQL"
+}
